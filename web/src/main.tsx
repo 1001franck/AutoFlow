e4a20f8821c780ffe8a11c500f1551fd381d5092
Ton Nom <harelfranck24@gmail.com>
2026-05-11 13:53:24 +0200

    Vfeat(web): setup Tailwind v4, Lexend Deca, dark/light mode, i18n FR/EN

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'next-themes';
+import './i18n';
+import './index.css';
+import App from './App';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 1, staleTime: 30_000 },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
